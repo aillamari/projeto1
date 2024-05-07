fb128925fc6e8ba23d314eaf20efbab7c8eafe9f
aillamari <127351793+aillamari@users.noreply.github.com>
2024-05-07 07:52:53 -0300

    C

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ for(let i =0;i<botoes.length;i++){
  }
 }
  const contador = document.querySelectorAll(".contador")
- const tempoObjetivo = new Date ("2024-10-05T00:00:00")
+ const tempoObjetivo1 = new Date ("2024-10-05T00:00:00")
  let tempoAtual = new Date()
 
  contador[0].textContent = ((tempoObjetivo1 - tempoAtual)/1000/60/60/24)
