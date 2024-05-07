6f4240921c2b081ad417b5cfb861313556506eb0
aillamari <127351793+aillamari@users.noreply.github.com>
2024-05-07 07:38:17 -0300

    m5we

--- main.js
+++ main.js
@@ -11,4 +11,8 @@ for(let i =0;i<botoes.length;i++){
         textos[i].classList.add("ativo");
  }
 }
+ const contador = document.querySelectorAll(".contador")
+ consttempoObjetivo = new Date ("2024-10-05T00:00:00")
+ lettempoAtual = new Date ()
 
+ contador[0].textContent = ((tempoObjetivo1 - tempoAtual)/1000/60/60/24)
