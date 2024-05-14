6f1608e13b7c49b2da65006d70c93553ca79a840
aillamari <127351793+aillamari@users.noreply.github.com>
2024-05-14 08:02:16 -0300

    ççmç

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ for(let i =0;i<botoes.length;i++){
         textos[i].classList.add("ativo");
  }
 }
- const contador = document.querySelectorAll(".contador")
- const tempoObjetivo1 = new Date ("2024-10-05T00:00:00")
- const tempoObjetivo2 = new Date ("2024-12-05T00:00:00")
- const tempoObjetivo3 = new Date ("2024-02-05T00:00:00")
- const tempoObjetivo4 = new Date ("2024-02-05T00:00:00")
+ const contador = document.querySelectorAll(".contador");
+ const tempoObjetivo1 = new Date ("2024-10-05T00:00:00");
+ const tempoObjetivo2 = new Date ("2024-12-05T00:00:00");
+ const tempoObjetivo3 = new Date ("2024-02-05T00:00:00");
+ const tempoObjetivo4 = new Date ("2024-02-05T00:00:00");
  let tempoAtual = new Date()
 
  contador[0].textContent = calculaTempo(tempoObjetivo1)
