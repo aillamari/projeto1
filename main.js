bb57d7d6b84a1c1d8b60d218a722875bed6080e8
aillamari <127351793+aillamari@users.noreply.github.com>
2024-05-14 08:01:45 -0300

    1vv14c

--- main.js
+++ main.js
@@ -13,6 +13,9 @@ for(let i =0;i<botoes.length;i++){
 }
  const contador = document.querySelectorAll(".contador")
  const tempoObjetivo1 = new Date ("2024-10-05T00:00:00")
+ const tempoObjetivo2 = new Date ("2024-12-05T00:00:00")
+ const tempoObjetivo3 = new Date ("2024-02-05T00:00:00")
+ const tempoObjetivo4 = new Date ("2024-02-05T00:00:00")
  let tempoAtual = new Date()
 
  contador[0].textContent = calculaTempo(tempoObjetivo1)
